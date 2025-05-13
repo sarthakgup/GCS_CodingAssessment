7aba8875616000d92fa49b9bff9386682eebe5ca
sarthakgup <sarthakgupta@outlook.com>
2025-05-13 18:36:43 -0500

    reorg

--- Activity2/sortAndFindMedian.cpp
+++ Activity2/sortAndFindMedian.cpp
@@ -2,23 +2,7 @@
 #include <vector>
 using namespace std;
 
-double sortAndFindMedian(vector<int>& numbers)
-{
-    //bubble sorting
-    sort(numbers);  //CALL sort(numbers)
-    
-    int n = numbers.size(); //DEFINE n as length of numbers
-    
-    if(n % 2 == 0)  //IF n mod 2 == 0
-    {
-        return (numbers[n / 2 - 1] + numbers[n / 2]) / 2.0;
-    }
-    else
-    {
-        return numbers[n / 2];
-    }
-}
-
+//bubble sort (O(N^2))
 void sort(vector<int> &numbers)
 {
     int n = numbers.size();
@@ -36,6 +20,23 @@ void sort(vector<int> &numbers)
     }
 }
 
+double sortAndFindMedian(vector<int>& numbers)
+{
+    //bubble sorting
+    sort(numbers);  //CALL sort(numbers)
+    
+    int n = numbers.size(); //DEFINE n as length of numbers
+    
+    if(n % 2 == 0)  //IF n mod 2 == 0
+    {
+        return (numbers[n / 2 - 1] + numbers[n / 2]) / 2.0;
+    }
+    else
+    {
+        return numbers[n / 2];
+    }
+}
+
 int main()
 {
     vector<int> nums = {7, 1, 3, 5, 2};
@@ -45,3 +46,62 @@ int main()
     
     return 0;
 }
+
+/*
+UNUSED CODE FUNCTIONS: (to demonstrate thought process later)
+
+
+void sort(vector<int> &numbers)
+{
+    //quick select sort to find the median
+
+}
+
+
+/quick select (since we only want to select the median) (O(n))
+//quickselect partition:
+int partition(vector<int> &nums, int left, int right)
+{
+    int pivot = nums[right];
+    int i = left;
+
+    for(int j = left; j < right; ++j)
+    {
+        if(nums[j] < pivot)
+        {
+            swap(nums[i], nums[j]);
+            ++i;
+        }
+    }
+
+    swap(nums[i], nums[right]);
+    return i;
+}
+
+//quickselect sort algorithm:
+int quickSelect(vector<int>& nums, int left, int right, int k)
+{
+    if (left == right)
+    {
+        return nums[left];
+    }
+
+    int pivotIndex = left + rand() % (right - left + 1);
+    swap(nums[pivotIndex], nums[right]); // Move pivot to end
+    pivotIndex = partition(nums, left, right);
+
+    if (k == pivotIndex)
+    {
+        return nums[k];
+    }
+    else if (k < pivotIndex)
+    {
+        return quickSelect(nums, left, pivotIndex - 1, k);
+    }
+    else
+    {
+        return quickSelect(nums, pivotIndex + 1, right, k);
+    }
+}
+
+*/
